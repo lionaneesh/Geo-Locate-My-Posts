0003c07af9d680428c2fbca93ddf44955e3bbeb1
Aneesh Dogra <lionaneesh@gmail.com>
2012-08-20 00:01:40 +0530

    Release v0.2 Final.

--- add-location.php
+++ add-location.php
@@ -1,55 +1,62 @@
 <?php
 /**
  * @package 
- * @version 0.1
+ * @version 0.2
  */
 /*
 Plugin Name: Geolocate My Posts
-Description: A Wordpress plugin that tags the location of the post using Google Maps.
+Description: A Wordpress plugin that tags the location of your posts using the Google Maps API.
 Author: Aneesh Dogra
-Version: 0.1
-Author URI: https://plus.google.com/109031586756412438081
+Version: 0.2
+Author URI: anee.me
 */
 
-if ( !defined( 'GMAPS_IMAGE_WIDTH'  ) ) define( 'GMAPS_IMAGE_WIDTH',  '400' );
-if ( !defined( 'GMAPS_IMAGE_HEIGHT' ) ) define( 'GMAPS_IMAGE_HEIGHT', '200' );
+if (!get_option('geolocate_my_posts_options'))
+	add_option('geolocate_my_posts_options', array("gmaps_image_width" => "400", "gmaps_image_height" => "200"));
 
-function get_lat_lon() {
+function get_lat_lon($hook) {
+	if ( 'post-new.php' != $hook and 'post.php' != $hook ) { return; }
+	
 	wp_enqueue_script( 'geolocation', plugins_url( '/js/geolocate.js', __FILE__ ), array('jquery') );
 }
 
 function save_lat_lon() {
-	add_option( 'latitude',  $_POST['lat'] );
-	add_option( 'longitude', $_POST['lon'] );
+	set_transient( 'latitude',  $_POST['lat'], 10); // expire in 10 secs
+	set_transient( 'longitude', $_POST['lon'], 10);
 }
 
 function addLocation( $data ) {
-	$lat = get_option( 'latitude',  '' );
-	$lon = get_option( 'longitude', '' );
-	if ( $data['post_status'] != 'publish' or
+	$lat = get_transient( 'latitude',  '' );
+	$lon = get_transient( 'longitude', '' );
+	if ( $data['post_status'] != 'publish' or // only append data when publishing the content. It avoids duplicates
 		 $lat == '' or $lon == '' ) {
 		return $data;
 	}
+	$options_array 			= get_option('geolocate_my_posts_options');
 	$gmaps_out 				= '';
 	$out 					= '';
 	$gmap_get_location_url  = "https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lon&sensor=false";
-	$gmap_image_url 		= "http://maps.googleapis.com/maps/api/staticmap?center=$lat,$lon&zoom=11&size=".GMAPS_IMAGE_WIDTH."x".GMAPS_IMAGE_HEIGHT."&sensor=false&markers=color:blue%7C$lat,$lon";
+	$gmap_image_url 		= "http://maps.googleapis.com/maps/api/staticmap?center=$lat,$lon&zoom=11&size=".$options_array['gmaps_image_width']."x".$options_array['gmaps_image_height']."&sensor=false&markers=color:blue%7C$lat,$lon";
 	$gmap_location_url 		= "https://maps.google.com/maps?q=$lat,$lon&num=1&t=h&z=12&iwloc=A";
+	$fp 	   				= fopen($gmap_get_location_url, "r");
 
-	$fp 	   = fopen($gmap_get_location_url, "r");
+	delete_transient('latitude');
+	delete_transient('longitude');
 
 	while ( !feof($fp) ) {
 		$gmaps_out .= fread($fp, 512);
 	}
 	fclose($fp);
+	
 	$geo_data = json_decode($gmaps_out, True);
+	
 	if ( $geo_data['status'] == 'OK' and
      	 get_post_meta(get_the_ID(), '_location_added', true) == '' ) {
 			$location = $geo_data['results'][0]['formatted_address'];
 			$data['post_content'] .= <<<EOT
 <!-- location -->
 <div style="padding: 10px; margin-top: 20px;">
-	<img src="$gmap_image_url"><br />
+	<img src="$gmap_image_url"><br /><br />
 	<p style="padding = 10px;">
 		At <a href="$gmap_location_url" title="Location">$location</a>
 	</p>
@@ -60,7 +67,66 @@ EOT;
 	}
 	return $data;
 }
-add_action( 'admin_enqueue_scripts',  'get_lat_lon'  );
-add_action( 'wp_ajax_my_action'    ,  'save_lat_lon' );
-add_filter( 'wp_insert_post_data'  ,  'addLocation'  );
+
+function gmp_admin_init() {
+	register_setting( 'plugin_options', 'geolocate_my_posts_options', 'geolocate_my_posts_options_validate' );
+	add_settings_section('geolocate_my_post_settings', 'Geolocation Settings', 'geolocation_section_text', 'geolocate-my-posts');
+	add_settings_field('gmaps_image_width', 'Width of Map Tile (in px)', 'gmaps_image_width_string', 'geolocate-my-posts', 'geolocate_my_post_settings');
+	add_settings_field('gmaps_image_height', 'Height of Map Tile (in px)', 'gmaps_image_height_string', 'geolocate-my-posts', 'geolocate_my_post_settings');
+}
+
+function gmaps_image_width_string() {
+$options = get_option('geolocate_my_posts_options');
+echo "<input id='plugin_text_string' name='geolocate_my_posts_options[gmaps_image_width]' size='40' type='text' value='{$options['gmaps_image_width']}' />";
+}
+
+function gmaps_image_height_string() {
+$options = get_option('geolocate_my_posts_options');
+echo "<input id='plugin_text_string' name='geolocate_my_posts_options[gmaps_image_height]' size='40' type='text' value='{$options['gmaps_image_height']}' />";
+}
+
+function geolocate_my_posts_options_validate($input) {
+	$newinput['gmaps_image_width']  = trim($input['gmaps_image_width']);
+	$newinput['gmaps_image_height'] = trim($input['gmaps_image_height']);
+
+	if(!preg_match('/\d+/', $newinput['gmaps_image_width'])) {
+		$newinput['gmaps_image_width'] = '50';
+	}
+	if(!preg_match('/\d+/', $newinput['gmaps_image_height'])) {
+		$newinput['gmaps_image_height'] = '50';
+	}
+
+	return $newinput;
+}
+
+function geolocation_section_text() {
+	echo '<p>Settings for the Map tile.</p>';
+}
+function gmp_plugin_menu() { // gmp == Geolocate My Posts
+add_options_page('Post Geolocation', 'Post Geolocation Settings', 'manage_options', 'geolocatePosts', 'gmp_plugin_options');
+}
+
+function gmp_plugin_options() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+?>
+<div>
+<h2>Geolocate My Posts</h2>
+Options related to Geolocate my posts plugin.
+<form action="options.php" method="post">
+<?php settings_fields('plugin_options'); ?>
+<?php do_settings_sections('geolocate-my-posts'); ?>
+
+<input name="Submit" type="submit" value="<?php esc_attr_e('Save Changes'); ?>" />
+</form></div>
+
+<?php
+}
+
+add_action( 'admin_enqueue_scripts',  'get_lat_lon'     );
+add_action( 'wp_ajax_save_lat_lon' ,  'save_lat_lon'    );
+add_filter( 'wp_insert_post_data'  ,  'addLocation'     );
+add_action( 'admin_menu'		   ,  'gmp_plugin_menu' );
+add_action( 'admin_init'           ,  'gmp_admin_init'  );
 ?>
--- readme.txt
+++ readme.txt
@@ -1,55 +1,46 @@
 === Geolocate My Posts ===
-Contributors: Aneesh Dogra
+Contributors: lionaneesh
 Tags: geo-locate, location, gmaps, Google Maps, add location to posts, locate my posts, geo-locate my posts
-Requires at least: 3.0
-Stable tag: 0.1
+Requires at least: 2.6
+Tested up to: 3.4.1
+Stable tag: 0.2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-A Wordpress plugin that tags the location of the post using Google Maps.
+A Wordpress plugin that tags the location of your posts using the Google Maps API.
 
 == Description ==
 
-A Wordpress plugin that adds location to your posts, using Geolocation and the Google Maps API.
-
-== Other Notes ==
-
-= To Do =
-[*] Improve CSS
+"Geolocate My Posts" adds location to your posts, including a nice map, using geolocation and the Google Maps API.
 
 == Installation ==
 
 You may either install the plugin via the in-built installer in WordPress or follow the manual method below:
 
-1. Upload the extracted `Geolocate-My-Posts` folder to the `/wp-content/plugins/` directory
+1. Upload the extracted `geolocate-my-posts` folder to the `/wp-content/plugins/` directory
 2. Activate the plugin through the 'Plugins' menu in WordPress
 3. Enjoy!
 
 == Frequently Asked Questions ==
 
-= 1. How do I change the Map's Image Width ?
-Open up add_location.php in a Text editor like Vim, Gedit, TextMate etc, and Change
+= How do I change the Map's Image Width ? =
+Go to Admin Panel->Settings->Post Geolocation Settings, and do the needful.
 
-if ( !defined( 'GMAPS_IMAGE_WIDTH'  ) ) define( 'GMAPS_IMAGE_WIDTH',  '400' );
-to
-if ( !defined( 'GMAPS_IMAGE_WIDTH'  ) ) define( 'GMAPS_IMAGE_WIDTH',  'YOUR_WIDTH_HERE' );
-
-Where YOUR_WIDTH_HERE is your desired width
-
-and Similarly, Change
-
-if ( !defined( 'GMAPS_IMAGE_HEIGHT' ) ) define( 'GMAPS_IMAGE_HEIGHT', '200' );
-to
-if ( !defined( 'GMAPS_IMAGE_HEIGHT' ) ) define( 'GMAPS_IMAGE_HEIGHT', 'YOUR_HEIGHT_HERE' );
-
-Where YOUR_HEIGHT_HERE is your desired Height.
-
-= 2. If I disable the plugin, will all the Locations from my Posts be removed? =
+= If I disable the plugin, will all the Locations from my Posts be removed? =
 No, The locations are appended in your posts as you click publish, thus, You have to devise a way to manually delete the locations if you wish to do so.
 
 == Screenshots ==
 
-Plugin in Action : https://docs.google.com/open?id=0B5gdkF1UMZiKaHRENTg0Y0ZjSWs
+* Plugin in Action
 
-== Change Log ==
+== Changelog ==
+
+= 0.2 =
+Final Release, No further development expected.
+
+* The geolocation javascript is only enqueued at pages where its necessary.
+* Use transients instead of options to store latitude, longitude.
+* Added customization.
 
 = 0.1 =
 Initial Release
